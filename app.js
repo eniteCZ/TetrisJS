74ed9e2fbb44f6b402e20cc64cf4ebeb90e340eb
eniteCZ <f.radmanovic@gmail.com>
2022-09-28 20:58:14 -0400

    Added display and start stop functionality

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const grid = document.querySelector('.grid');
     let squares = Array.from(document.querySelectorAll('.grid div'));
     const width = 10;
+    let timerID; 
     const ScoreDisplay = document.querySelector('#score');
-    const StartBtn = document.querySelector('#start-btn');
-
+    let StartBtn = document.querySelector('#start-btn');
+    
     const lTetr = [
         [1, width+1, width*2+1, 2],
         [width, width+1, width+2, width*2+2],
@@ -33,12 +34,31 @@ document.addEventListener('DOMContentLoaded', ()=>{
         [1,width,width+1,width*2+1]
     ];
 
-    const Tetrs = [lTetr,tTetr,sqTetr,iTetr]
+    const zTetr = [
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1]
+      ];
 
-    let currentPos = 4
-    let currentRot = 0
-    let random = Math.floor(Math.random()*Tetrs.length)
-    let currentTetr = Tetrs[random][currentRot]
+    const Tetrs = [lTetr,tTetr,sqTetr,iTetr,zTetr];
+
+    let currentPos = 4;
+    let currentRot = 0;
+    let random = Math.floor(Math.random()*Tetrs.length);
+    let currentTetr = Tetrs[random][currentRot];
+    const displaySquares = document.querySelectorAll('.minigrid div');
+    const displayWidth = 4;
+    let displayIndex = 0;
+    let nextRandom = 0;
+
+    const upNext = [
+        [1, displayWidth+1, displayWidth*2+1, 2],
+        [1,displayWidth+1,displayWidth*2+1,displayWidth*3+1],
+        [0,1,displayWidth,displayWidth+1],
+        [1,displayWidth,displayWidth+1,displayWidth+2],
+        [0,displayWidth,displayWidth+1,displayWidth*2+1]
+    ];
 
     //draw tetrimino
     function draw(){
@@ -64,10 +84,12 @@ document.addEventListener('DOMContentLoaded', ()=>{
         if(currentTetr.some(index => squares[currentPos + index + width].classList.contains('taken'))) {
             currentTetr.forEach(index => squares[currentPos + index].classList.add('taken'))
             
-            random = Math.floor(Math.random()*Tetrs.length)
+            random = nextRandom
+            nextRandom = Math.floor(Math.random()*Tetrs.length)
             currentTetr = Tetrs[random][currentRot]
             currentPos = 4
             draw()
+            displayShape()
         }
     }
 
@@ -82,12 +104,63 @@ document.addEventListener('DOMContentLoaded', ()=>{
         draw()
     }
 
+    function moveRight(){
+        undraw()
+        let isAtRightEdge = currentTetr.some(index => (currentPos + index) % width === width - 1)
+        if(!isAtRightEdge) currentPos +=1
+        if(currentTetr.some(index => squares[currentPos+index].classList.contains('taken'))){
+            currentPos -=1
+        }
+
+        draw()
+    }
+
+    function rotate() {
+        undraw()
+        currentRot ++
+        if(currentRot === currentTetr.length){
+            currentRot = 0
+        }
+        currentTetr = Tetrs[random][currentRot]
+        draw()
+    }
+
     function control(e){
         if(e.keyCode === 37) {
             moveLeft()
+        } else if (e.keyCode ===38){
+            rotate()
+        } else if (e.keyCode === 39){
+            moveRight()
+        }else if (e.keyCode === 40){
+            moveDown()
         }
     }
 
-    timerID = setInterval(moveDown, 1000);
-    document.addEventListener('keyup', control)
+    function displayShape(){
+        displaySquares.forEach( square =>{
+            square.classList.remove('tetr')
+        })
+        upNext[nextRandom].forEach(index => {
+            displaySquares[displayIndex + index].classList.add('tetr')
+        })
+    }
+
+    //timerID = setInterval(moveDown, 1000);
+
+    StartBtn.addEventListener('click', ()=>{
+        if (timerID){
+            clearInterval(timerID)
+            timerID = null
+        }else{
+            draw()
+            timerID = setInterval(moveDown,1000)
+            nextRandom = Math.floor(Math.random()*Tetrs.length)
+            displayShape()
+        }
+    })
+
+    
+    
+    document.addEventListener('keyup', control);
 })
